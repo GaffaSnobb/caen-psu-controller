7b6d7e19e8b5220fd5f0af74fabc0249202e33cd
Jon Dahl <jon@dira.no>
2023-05-16 15:17:35 +0200

    Add more mappers

--- mappers.py
+++ mappers.py
@@ -1,3 +1,5 @@
+from utilities import response_stripper
+
 def mst_mapper(response: bytes):
     """
     TODO: Map the 2 digit hex status register response to the correct
@@ -11,4 +13,31 @@ def mst_mapper(response: bytes):
     1: fault
     0: module on
     """
+    response = response_stripper(response=response)
+    response = int(response, 16)
+    if response & 0b00000001:
+        print("The module is enabled and correctly regulating output current.")
+    else:
+        print("The module is disabled.")
+    if response & 0b00000010:
+        print("The module has experienced a fault.")
+    else:
+        print("No fault.")
+    if response & 0b00000100:
+        print("A DC-Link under-voltage condition has been recognized.")
+    else:
+        print("No under-voltage")
+    if response & 0b00001000:
+        print("A MOSFET over-temperature condition has been experienced.")
+    else:
+        print("MOSFET temperature OK.")
+    if response & 0b00010000:
+        print("A shunt case over-temperature condition has been experienced.")
+    else:
+        print("Shunt case temperature OK")
+    if response & 0b00100000:
+        print("The corresponding external interlock trips.")
+    else:
+        print("No external interlock.")
+
     return response
