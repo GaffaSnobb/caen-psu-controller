5574cad1abdf9865fd03fc86d86cd74cee645e7c
Jon Dahl <jon@dira.no>
2023-05-22 13:20:23 +0200

    Extend commands dict with all commands from the documentation

--- commands.py
+++ commands.py
@@ -4,12 +4,6 @@ from mappers import mst_mapper
 no_mapper = lambda response: response
 
 commands_0520: dict[str, Commands] = {  # Commands for the Easy-Driver 0520.
-    "MST": Commands(
-        description = "Read module internal status register",
-        is_read = True,
-        is_write = False,
-        response_mapper = mst_mapper,
-    ),
     "reboot": Commands(
         description = (
             "Reboot module. This command is different from the other commands"
@@ -21,6 +15,12 @@ commands_0520: dict[str, Commands] = {  # Commands for the Easy-Driver 0520.
         is_write = True,
         response_mapper = no_mapper,
     ),
+    "MST": Commands(
+        description = "Read module internal status register",
+        is_read = True,
+        is_write = False,
+        response_mapper = mst_mapper,
+    ),
     "MON": Commands(
         description = "Turn on output driver",
         is_read = False,
@@ -29,6 +29,102 @@ commands_0520: dict[str, Commands] = {  # Commands for the Easy-Driver 0520.
     ),
     "MOFF": Commands(
         description = "Turn off output driver",
+        is_read = False,
+        is_write = True,
+        response_mapper = no_mapper,
+    ),
+    "FDB": Commands(
+        description = "Feedback command",
+        is_read = False,
+        is_write = True,
+        response_mapper = no_mapper,
+    ),
+    "MRESET": Commands(
+        description = "Reset the module status register",
+        is_read = False,
+        is_write = True,
+        response_mapper = no_mapper,
+    ),
+    "MRG": Commands(
+        description = "Read selected EEPROM 'value' cell",
+        is_read = True,
+        is_write = False,
+        response_mapper = no_mapper,
+    ),
+    "MRI": Commands(
+        description = "Read output current value",
+        is_read = True,
+        is_write = False,
+        response_mapper = no_mapper,
+    ),
+    "MRID": Commands(
+        description = "Read module identification",
+        is_read = True,
+        is_write = False,
+        response_mapper = no_mapper,
+    ),
+    "MRM": Commands(
+        description = "Set output current value (ramp)",
+        is_read = False,
+        is_write = True,
+        response_mapper = no_mapper,
+    ),
+    "MRP": Commands(
+        description = "Read DC-Link voltage value",
+        is_read = True,
+        is_write = False,
+        response_mapper = no_mapper,
+    ),
+    "MRSR": Commands(
+        description = "Read current Slew Rate",
+        is_read = True,
+        is_write = False,
+        response_mapper = no_mapper,
+    ),
+    "MRT": Commands(
+        description = "Read output stage heatsink temperature",
+        is_read = True,
+        is_write = False,
+        response_mapper = no_mapper,
+    ),
+    "MRTS": Commands(
+        description = "Read regulation shunt temperature",
+        is_read = True,
+        is_write = False,
+        response_mapper = no_mapper,
+    ),
+    "MRV": Commands(
+        description = "Read output voltage value",
+        is_read = True,
+        is_write = False,
+        response_mapper = no_mapper,
+    ),
+    "MVER": Commands(
+        description = "Power Supply firmware version",
+        is_read = True,
+        is_write = False,
+        response_mapper = no_mapper,
+    ),
+    "MWG": Commands(
+        description = "Write selected EEPROM 'value' cell",
+        is_read = False,
+        is_write = True,
+        response_mapper = no_mapper,
+    ),
+    "MPUP": Commands(
+        description = "Reload EEPROM values in DSP",
+        is_read = False,
+        is_write = True,
+        response_mapper = no_mapper,
+    ),
+    "MWSR": Commands(
+        description = "Write current Slew Rate",
+        is_read = False,
+        is_write = True,
+        response_mapper = no_mapper,
+    ),
+    "MWI": Commands(
+        description = "Set output current value (no ramp)",
         is_read = False,
         is_write = True,
         response_mapper = no_mapper,
