5abc126df88725f7d6b37c0e48f53e1ba800e5fa
Jon Dahl <jon@dira.no>
2023-05-26 14:12:01 +0200

    Improve response handling
    The program listens for a response until the termination character is received and then returns the response.

--- caen_easy_driver/controller.py
+++ caen_easy_driver/controller.py
@@ -1,33 +1,34 @@
 import socket, time
 from .commands import commands_0520
 
-def reboot_psu(server_ip: str) -> bytes:
+def reboot_psu(ip: str) -> bytes:
     """
     Reboot command is simply this sequence of 9-bytes commands sent via
     UDP to port 30704 with a 100ms delay in between the commands.
     """
-    server_port: int = 30704
+    port: int = 30704
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
-        sock.sendto(b'\x19\x04\x00\x00\x00\x04\x00\x00\x00', (server_ip, server_port))
+        sock.sendto(b'\x19\x04\x00\x00\x00\x04\x00\x00\x00', (ip, port))
         time.sleep(0.1)
-        sock.sendto(b'\x1A\x04\x00\x00\x00\x00\x00\x00\x00', (server_ip, server_port))
+        sock.sendto(b'\x1A\x04\x00\x00\x00\x00\x00\x00\x00', (ip, port))
         time.sleep(0.1)
-        sock.sendto(b'\x1B\x04\x00\x00\x00\x00\x00\x00\x00', (server_ip, server_port))
+        sock.sendto(b'\x1B\x04\x00\x00\x00\x00\x00\x00\x00', (ip, port))
         time.sleep(0.1)
-        sock.sendto(b'\x1B\x04\x00\x00\x00\x04\x00\x00\x00', (server_ip, server_port))
+        sock.sendto(b'\x1B\x04\x00\x00\x00\x04\x00\x00\x00', (ip, port))
         data, server = sock.recvfrom(4096)
 
     return data
 
 class CaenEasyDriverControl:
-    def __init__(self, server_ip: str, server_port: int):
-        self.server_ip = server_ip
-        self.server_port = server_port
+    def __init__(self, ip: str, port: int):
+        self.ip = ip
+        self.port = port
         self.sock = None
 
     def __enter__(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.connect((self.server_ip, self.server_port))
+        self.sock.connect((self.ip, self.port))
+        self.sock.setblocking(1)
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
@@ -36,9 +37,14 @@ class CaenEasyDriverControl:
 
     def send_command(self, command: str) -> bytes:
         """
-        Send a command, and return the answer
-        from a Caen Easy-Driver 0520. Maybe it works for other Caen PSUs?
+        Send a command, and return the answer from a Caen Easy-Driver
+        0520. This function keeps listening until the termination
+        character b'\r' has been reached. No new command will be sent
+        before the termination character in the response of the previous
+        command has been received. Maybe it works for other Caen PSUs?
         I dont know, havent tested it!
+
+        NOTE: Maybe add a timeout for sock.recv?
 
         The status register value can be directly read by users using the
         `MST\r` command. The returned item is a 2-digit hexadecimal ASCII
@@ -59,33 +65,40 @@ class CaenEasyDriverControl:
 
         if command == "reboot":
             """
-            The reboot command is different from the other commands since it
+            The reboot command is different from the other commands. It
             is sent by UDP to port 30704.
             """
-            return reboot_psu(server_ip=self.server_ip)
+            return reboot_psu(ip=self.ip)
 
+        buffer = b""
         self.sock.sendall((command + '\r').encode())
-        response: bytes = self.sock.recv(4096)
+        while True:
+            """
+            Listen for a response until the termination character has
+            been received.
+            """
+            response = self.sock.recv(4096)
+            buffer += response
+            if buffer.endswith(b"\r"): break
 
-        return response
-
+        return buffer
 
 if __name__ == "__main__":
-    server_ip = '192.168.0.222' # Caen Easy-Driver 0520 dev
-    server_port = 10001
+    ip = '192.168.0.222' # Caen Easy-Driver 0520 dev
+    port = 10001
 
     response: bytes = send_command(
         # command = "reboot",
         command = "MOFF",
-        server_ip = server_ip,
-        server_port = server_port
+        ip = ip,
+        port = port
     )
     response: bytes = send_command(
         # command = "reboot",
         # command = "MOFF",
         command = "MST",
-        server_ip = server_ip,
-        server_port = server_port
+        ip = ip,
+        port = port
     )
 
     print(commands_0520["MST"].response_mapper(response))
