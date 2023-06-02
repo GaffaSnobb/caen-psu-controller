7fd92c45983084057626cebf196b361d67dca08c
Jon Dahl <jon@dira.no>
2023-06-02 15:06:40 +0200

    Add option for extracting status from the mst hex value

--- caen_easy_driver/mappers.py
+++ caen_easy_driver/mappers.py
@@ -2,7 +2,7 @@ from .utilities import response_stripper
 
 def mst_mapper(
     response: bytes,
-    verbose: bool = False
+    is_enabled: bool = False
 ):
     """
     TODO: Map the 2 digit hex status register response to the correct
@@ -16,33 +16,33 @@ def mst_mapper(
     1: fault
     0: module on
     """
+    if is_enabled:
+        return response & 0b00000001
+    
     response = response_stripper(response=response)
     response = int(response, 16)
-    if not verbose: return response
-    
-    if response & 0b00000001:
-        print("The module is enabled and correctly regulating output current.")
-    else:
-        print("The module is disabled.")
-    if response & 0b00000010:
-        print("The module has experienced a fault.")
-    else:
-        print("No fault.")
-    if response & 0b00000100:
-        print("A DC-Link under-voltage condition has been recognized.")
-    else:
-        print("No under-voltage")
-    if response & 0b00001000:
-        print("A MOSFET over-temperature condition has been experienced.")
-    else:
-        print("MOSFET temperature OK.")
-    if response & 0b00010000:
-        print("A shunt case over-temperature condition has been experienced.")
-    else:
-        print("Shunt case temperature OK")
-    if response & 0b00100000:
-        print("The corresponding external interlock trips.")
-    else:
-        print("No external interlock.")
+    return response
+    # else:
+    #     print("The module is disabled.")
+    # if response & 0b00000010:
+    #     print("The module has experienced a fault.")
+    # else:
+    #     print("No fault.")
+    # if response & 0b00000100:
+    #     print("A DC-Link under-voltage condition has been recognized.")
+    # else:
+    #     print("No under-voltage")
+    # if response & 0b00001000:
+    #     print("A MOSFET over-temperature condition has been experienced.")
+    # else:
+    #     print("MOSFET temperature OK.")
+    # if response & 0b00010000:
+    #     print("A shunt case over-temperature condition has been experienced.")
+    # else:
+    #     print("Shunt case temperature OK")
+    # if response & 0b00100000:
+    #     print("The corresponding external interlock trips.")
+    # else:
+    #     print("No external interlock.")
 
     return response
